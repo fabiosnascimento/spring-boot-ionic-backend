36c7f8f90cd378e98c1d5251f1ef83ef47840d9c
Fabio Nascimento <fabiogtr@hotmail.com>
2021-06-25 16:30:21 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/fabionascimento/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.fabionascimento.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fabionascimento.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
